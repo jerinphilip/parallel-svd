966a610424b1938545d2ddd386ffd1ea5eddebb1
Jerin Philip <jerin.philip@research.iiit.ac.in>
2018-04-19 10:44:26 +0530

    Okay, * is elementwise multiplication. Need to come up with something new for matrix multiplication

--- src/ops.h
+++ src/ops.h
@@ -27,22 +27,24 @@ tuple<Tensor, Tensor, Tensor> SVD(const Tensor A){
 }
 */
 
-#define CPU_ELEMENT_WISE(op) \
-    CPUTensor operator op(const CPUTensor A, const CPUTensor B){ \
-        /* Assertions */                                         \
-        assert (A.rows == B.rows and A.cols == B.cols);          \
-        CPUTensor C(A.rows, A.cols);                             \
-                                                                 \
-        /* Settling for two dimensions now */                    \
-        for(int i = 0; i < A.rows; i++){                         \
-            for(int j=0; j < A.cols; j++){                       \
-                C(i, j) = A(i, j) op B(i, j);                   \
-            }                                                    \
-        }                                                        \
-        return C;                                                \
+#define CPU_ELEMENT_WISE(op)                                      \
+    CPUTensor operator op(const CPUTensor A, const CPUTensor B){  \
+        /* Assertions */                                          \
+        assert (A.rows == B.rows and A.cols == B.cols);           \
+        CPUTensor C(A.rows, A.cols);                              \
+                                                                  \
+        /* Settling for two dimensions now */                     \
+        for(int i = 0; i < A.rows; i++){                          \
+            for(int j=0; j < A.cols; j++){                        \
+                C(i, j) = A(i, j) op B(i, j);                     \
+            }                                                     \
+        }                                                         \
+        return C;                                                 \
     }
 
 CPU_ELEMENT_WISE(+)
 CPU_ELEMENT_WISE(-)
+CPU_ELEMENT_WISE(*)
+CPU_ELEMENT_WISE(/)
 
 #endif
