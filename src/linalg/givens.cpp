513b8883de3b19f9a9fc6fa2c9111629f25f8cef
Jerin Philip <jerin.philip@students.iiit.ac.in>
2018-05-01 18:11:38 +0530

    templating givens

--- src/linalg/givens.cpp
+++ src/linalg/givens.cpp
@@ -3,7 +3,8 @@
 /* takes 1x2 column vector x and returns givens matrix to be multiplied from 
    left, takes diagonal location of pair to be rotated as parameter, as well as
    size of givens matrix */
-CPUTensor givens(CPUTensor x, int pos, int size) {
+template <class _Tensor>
+_Tensor givens(_Tensor x, int pos, int size) {
     double x1, x2;
     x1 = x(0, 0);
     x2 = x(1, 0);
@@ -11,7 +12,7 @@ CPUTensor givens(CPUTensor x, int pos, int size) {
     double xnorm;
     xnorm = norm(x);
     
-    CPUTensor G(2, 2);
+    _Tensor G(2, 2);
     G(0, 0) = x1/xnorm;
     G(0, 1) = x2/xnorm;
     G(1, 0) = (-1*x2)/xnorm;
