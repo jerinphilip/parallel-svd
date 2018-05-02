4fe0219f16d8a44906086640894eb39f6ab4dec9
octopada <gogollian@gmail.com>
2018-05-02 20:34:27 +0530

    classify

--- src/model/model.h
+++ src/model/model.h
@@ -1,10 +1,13 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#include <float.h>
 #include <map>
 #include <vector>
+#include "tensor/indexing.h"
 #include "linalg/linalg.h"
 #include "model/dataset.h"
+
 
 template <class Tensor>
 struct decomposition {
@@ -26,16 +29,36 @@ struct model {
         }
     }
 
-    std::vector<int> classify(Tensor T){
+    int classify(Tensor T){
+        int k = 10;
         /* 
          * Assume the columns of tensor contain the flattened image.
          * Now use matrix multiplications with U, and back, then norm to
          * compute the loss.
          * Take argmin of loss. Implement argmin at the GPU? Speedup.
          */
+        Tensor alpha;
+        Tensor Uk;
+        Tensor z;
 
+        int digit;
+        double min_loss = DBL_MAX;
+        double loss;
+        for(int i = 0; i < 10; i++) {
+            auto decomp = params[i];
+            block kb = block(0, decomp.U.rows)(0, k);
+            Uk = slice(decomp.U, kb);
+            alpha = Uk.transpose()*T;
+            z = Uk*alpha;
+              
+            loss = norm(z-T);
+            if(loss < min_loss) {
+                min_loss = loss;
+                digit = i;
+            }
+        }
+        return digit;
     }
-
 };
 
 #endif
