46d4d58276ad497461d1e5a7e87de40fe246909d
octopada <gogollian@gmail.com>
2018-05-03 16:20:49 +0530

    untested loss

--- src/model/model.h
+++ src/model/model.h
@@ -12,6 +12,29 @@
 template <class Tensor>
 struct decomposition {
     Tensor U, Sigma, V;
+    Tensor loss(Tensor Z, int k) {
+        /* assuming columns of Z are input samples */
+        std::vector<double> norms;
+        double *norms_array;
+        
+        block kb = block(0, (*this).U.rows)(0, k);
+        Tensor Uk = slice((*this).U, kb);
+        
+        Tensor Z_prime = Uk*Uk.transpose*Z;
+        for (int i = 0; i < Z.cols; i++) {
+            block col = block(0, Z.rows)(i, i+1);
+            Tensor z = slice(Z, col);
+            Tensor z_prime = slice(Z_prime, col);
+            norm_z = norm(z-z_prime);
+            norms.push_back(norm_z);
+        }
+        norms_array = &norms[0];
+        Tensor norms_tensor(1, Z.cols);
+        norms_tensor.from_array(norms_array, 1, Z.cols);
+        
+        /* returns a 1*n (where n is the number of samples) tensor of norms */
+        return norms_tensor;
+    }
 };
 
 template <class Tensor>
@@ -20,6 +43,7 @@ struct model {
     model(dataset<Tensor> &ds){
         for(auto p: ds.data){
             std::cout << "SVD : " << p.first << "\n";
+//            print_m(p.second);
             auto dt = svd(p.second);
             decomposition<Tensor> decomp;
             decomp.U = std::get<0>(dt);
@@ -30,34 +54,13 @@ struct model {
     }
 
     int classify(Tensor T){
-        int k = 10;
         /* 
          * Assume the columns of tensor contain the flattened image.
          * Now use matrix multiplications with U, and back, then norm to
          * compute the loss.
          * Take argmin of loss. Implement argmin at the GPU? Speedup.
          */
-        Tensor alpha;
-        Tensor Uk;
-        Tensor z;
-
-        int digit;
-        double min_loss = DBL_MAX;
-        double loss;
-        for(int i = 0; i < 10; i++) {
-            auto decomp = params[i];
-            block kb = block(0, decomp.U.rows)(0, k);
-            Uk = slice(decomp.U, kb);
-            alpha = Uk.transpose()*T;
-            z = Uk*alpha;
-              
-            loss = norm(z-T);
-            if(loss < min_loss) {
-                min_loss = loss;
-                digit = i;
-            }
-        }
-        return digit;
+         return 0;
     }
 };
 
